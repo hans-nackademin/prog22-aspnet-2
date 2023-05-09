2643325889bc1db2f8716ff1fec27b357b1eeaf5
Hans Mattin-Lassei <hans.mattin-lassei@epndata.com>
2023-05-09 13:26:09 +0200

    lektion 5 (external authentication)

--- lektion-5/WebApp/Controllers/LoginController.cs
+++ lektion-5/WebApp/Controllers/LoginController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication.Facebook;
 using Microsoft.AspNetCore.Authentication.Google;
+using Microsoft.AspNetCore.Authentication.MicrosoftAccount;
 using Microsoft.AspNetCore.Authentication.Twitter;
 using Microsoft.AspNetCore.Mvc;
 using WebApp.Models.ViewModels;
@@ -30,34 +31,36 @@ public class LoginController : Controller
 
     #endregion
 
-    public async Task Facebook() => await HttpContext.ChallengeAsync(
-        FacebookDefaults.AuthenticationScheme, 
-        new AuthenticationProperties { RedirectUri = Url.Action("ExternalAuthentication") });
+    public async Task Facebook() => await HttpContext.ChallengeAsync(FacebookDefaults.AuthenticationScheme, new AuthenticationProperties { RedirectUri = Url.Action("ExternalAuthentication") });
+    public async Task Google() => await HttpContext.ChallengeAsync(GoogleDefaults.AuthenticationScheme, new AuthenticationProperties { RedirectUri = Url.Action("ExternalAuthentication") });
+    public async Task MicrosoftAccount() => await HttpContext.ChallengeAsync(MicrosoftAccountDefaults.AuthenticationScheme, new AuthenticationProperties { RedirectUri = Url.Action("ExternalAuthentication") });
 
-    public async Task Google() => await HttpContext.ChallengeAsync(
-        GoogleDefaults.AuthenticationScheme, 
-        new AuthenticationProperties { RedirectUri = Url.Action("ExternalAuthentication") });
-
-    public async Task Twitter() => await HttpContext.ChallengeAsync(
-        TwitterDefaults.AuthenticationScheme, 
-        new AuthenticationProperties { RedirectUri = Url.Action("ExternalAuthentication") });
+    //public async Task Twitter() => await HttpContext.ChallengeAsync(TwitterDefaults.AuthenticationScheme, new AuthenticationProperties { RedirectUri = Url.Action("ExternalAuthentication") });
 
 
     public async Task<IActionResult> ExternalAuthentication()
     {
         var result = await HttpContext.AuthenticateAsync(CookieAuthenticationDefaults.AuthenticationScheme);
-        if (!result.Succeeded)
+        if (result.Succeeded)
         {
-            ModelState.AddModelError("", "Unable to login");
-            return RedirectToAction("Index");
+            var claims = result.Principal.Identities.FirstOrDefault()?.Claims.Select(claim => new
+            {
+                claim.Issuer,
+                claim.Type,
+                claim.Value
+            });
+
+            return Json(claims);
         }
 
-        var claims = result.Principal.Identities.FirstOrDefault()?.Claims.Select(claim => new
-        {
-            claim.Type,
-            claim.Value
-        });
+        ModelState.AddModelError("", "Unable to login");
+        return RedirectToAction("Index");
 
-        return Json(claims);
+    }
+
+    public async Task<IActionResult> Logout()
+    {
+        await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+        return RedirectToAction("Index");
     }
 }
--- lektion-5/WebApp/Program.cs
+++ lektion-5/WebApp/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using WebApp.Contexts;
@@ -8,8 +9,37 @@ builder.Services.AddControllersWithViews();
 // Contexts
 builder.Services.AddDbContext<DataContext>(x => x.UseSqlServer(builder.Configuration.GetConnectionString("Sql")));
 
+
+// External Authentication
+builder.Services.AddAuthentication(x =>
+{
+    x.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    x.DefaultChallengeScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+})
+.AddCookie()
+.AddFacebook(x =>
+{
+    x.ClientId = builder.Configuration["Facebook:ClientId"]!;
+    x.ClientSecret = builder.Configuration["Facebook:ClientSecret"]!;
+})
+.AddGoogle(x =>
+{
+    x.ClientId = builder.Configuration["Google:ClientId"]!;
+    x.ClientSecret = builder.Configuration["Google:ClientSecret"]!;
+})
+.AddMicrosoftAccount(x =>
+{
+    x.ClientId = builder.Configuration["MicrosoftAccount:ClientId"]!;
+    x.ClientSecret = builder.Configuration["MicrosoftAccount:ClientSecret"]!;
+});
+//.AddTwitter(x =>
+//{
+//    x.ConsumerKey = builder.Configuration["Twitter:ConsumerKey"]!;
+//    x.ConsumerSecret = builder.Configuration["Twitter:ConsumerSecret"]!;
+//})
+
 // Identity
-builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<DataContext>();
+//builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<DataContext>();
 
 
 var app = builder.Build();
@@ -17,6 +47,9 @@ app.UseHsts();
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
+
+app.UseAuthentication();
+
 app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
