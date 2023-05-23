a9e1f2d56c63b8e73fcb364a32fee0451ebfdecf
Hans Mattin-Lassei <hans.mattin-lassei@epndata.com>
2023-05-23 10:01:36 +0200

    lektion 8 (testning)

---
+++ lektion-8/UnitTesting/ConsoleApp/Services/CalculatorService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.Services
+{
+    public class CalculatorService
+    {
+        public decimal Total { get; set; }
+
+        public void Add(decimal value)
+        {
+            Total += value;
+        }
+    }
+}
